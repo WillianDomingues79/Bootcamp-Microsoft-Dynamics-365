73dcca394aec9027bce57fac1c9d5f0e3fa4ed1a
WillianDomingues79 <willian.domingues3@gmail.com>
2023-05-15 20:55:40 -0300

    feat:Inclusao de + 2 soluções do desafio

---
+++ Solucoes C#/CachorroQuente/CachorroQuente/Program.cs
@@ -0,0 +1,29 @@
+﻿// Para ler e escrever dados em C#, utilizamos os seguintes métodos da classe Console:
+// - Console.ReadLine: lê UMA linha com dado(s) de Entrada (Inputs) do usuário;
+// - Console.WriteLine: imprime um texto de Saída (Output) e pulando uma linha.
+
+using System;
+
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+        string[] entrada = Console.ReadLine().Split();
+        int participantes = int.Parse(entrada[0]);
+        int cachorrosQuentes = int.Parse(entrada[1]);
+
+        //TODO: Calcular a média de cachorros-quentes consumidos pelos participantes.
+
+        //TODO: Imprimir a média com duas casas decimais.
+        //Dica: Pesquise sobre o método "ToString".
+        double media = ((double)participantes / cachorrosQuentes);
+        Console.WriteLine(media.ToString("F2"));
+
+        //OUTRA SOLUÇÃO
+        /*double result = ((double)participantes / cachorrosQuentes);
+        double resultado = Math.Round(result, 2);
+        Console.WriteLine(resultado);*/
+    }
+}
+
---
+++ Solucoes C#/NumerosAzarados/NumerosAzarados/Program.cs
@@ -0,0 +1,24 @@
+﻿// Para ler e escrever dados em C#, utilizamos os seguintes métodos da classe Console:
+// - Console.ReadLine: lê UMA linha com dado(s) de Entrada (Inputs) do usuário;
+// - Console.WriteLine: imprime um texto de Saída (Output) e pulando uma linha.
+
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        string numero = Console.ReadLine();
+
+        if (numero.Contains("13"))
+        {
+            Console.WriteLine("13 es de Mala Suerte");
+        }
+        else
+        {
+            Console.WriteLine($"{numero} NO es de Mala Suerte");
+        }
+        //TODO: Descubra e imprima se o número é azarado.
+        //Dica: O método "Contains" pode ser útil neste desafio.
+    }
+}
